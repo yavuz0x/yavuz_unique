7de18d3c1308ceead6e015052f14ab5640addd66
root <root@vmi1519792.contaboserver.net>
2024-02-27 00:00:47 +0100

    Add  dependency

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup, find_packages
-
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+kfrom setuptools import setup, find_packages
 
 setup(
     name='yavuz_unique',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=[
+        'my-notebook-app 0.1.0', # Gerekli bağımlılıkları buraya ekleyin
+    ],
     entry_points={
         'console_scripts': [
-            'yavuz-unique-game=yavuz_unique.game_logic:play_game',
+            'my-notebook-app=my_notebook_app.main:main',
         ],
     },
 )
+
