ddbc8d637a2423b5a3a361b9a63e298ccc9c41eb
root <root@vmi1519792.contaboserver.net>
2024-02-27 17:24:54 +0100

    Initial commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yavuz_unique',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'my-notebook-app==0.1.0',  # Gerekli bağımlılıkları buraya ekleyin
