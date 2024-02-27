d3d60ce6c6e24976c540d88b16b9b1703df70410
root <root@vmi1519792.contaboserver.net>
2024-02-27 17:19:08 +0100

    Güncellenmiş setup.py dosyası eklendi.

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='yavuz_unique',
     version='0.1.0',
-    author='Yavuz',  # Kendi adınızı buraya ekleyin
-    author_email='yavuz0x00@gmail.com',  # Kendi e-posta adresinizi buraya ekleyin
     packages=find_packages(),
     install_requires=[
         'my-notebook-app==0.1.0',  # Gerekli bağımlılıkları buraya ekleyin
@@ -15,4 +13,16 @@ setup(
         ],
     },
     url='https://github.com/yavuz0x/yavuz_unique',  # GitHub deposunun URL'sini ekleyin
+    author='Yavuz',
+    author_email='yavuz0x00@gmail.com',
+    description='A short description of your project',
+    long_description=open('README.md').read(),  # README.md dosyasının içeriğini uzun açıklama olarak kullanın
+    long_description_content_type='text/markdown',  # README.md dosyasının türünü belirtin
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        # Diğer sınıflandırmaları buraya ekleyin
+    ],
 )
