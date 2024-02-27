422bea39a882912349c9dc04763cb02c194e2115
root <root@vmi1519792.contaboserver.net>
2024-02-27 17:04:09 +0100

    Güncellenmiş setup.py dosyası eklendi.

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='yavuz_unique',
     version='0.1.0',
+    author='Yavuz',  # Kendi adınızı buraya ekleyin
+    author_email='yavuz0x00@gmail.com',  # Kendi e-posta adresinizi buraya ekleyin
     packages=find_packages(),
     install_requires=[
         'my-notebook-app==0.1.0',  # Gerekli bağımlılıkları buraya ekleyin
