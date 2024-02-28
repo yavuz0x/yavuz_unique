d8a927af7cc4d2b6a3b72d784fec2a3ab6b86cb0
root <root@vmi1519792.contaboserver.net>
2024-02-28 16:00:19 +0100

    Güncellenmiş setup.py dosyası eklendi.

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-# setup.py
 from setuptools import setup, find_packages
 
 setup(
     name='yavuz_unique',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     install_requires=[
-        'my-notebook-app==0.1.0',
+        'my-notebook-app==0.1.0',  # Gerekli bağımlılıkları buraya ekleyin
     ],
     entry_points={
         'console_scripts': [
@@ -16,7 +15,6 @@ setup(
     author='Yavuz',
     author_email='yavuz0x00@gmail.com',
     description='A short description of your project',
-    url='https://github.com/yavuz0x/yavuz_unique',  # Burada url ekledik
     bugtrack_url='https://github.com/yavuz0x/yavuz_unique',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -26,4 +24,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.8',
     ],
+    project_urls={
+        'Homepage': 'https://github.com/yavuz0x/yavuz_unique',
+        'Source': 'https://github.com/yavuz0x/yavuz_unique',
+    },
 )
