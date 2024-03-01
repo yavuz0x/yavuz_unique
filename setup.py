bf4fd1029dd83185914f5d917f937657ae9c836a
root <root@vmi1519792.contaboserver.net>
2024-03-01 19:56:51 +0100

    Initial commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yavuz_unique',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(),
     install_requires=[
         'cann_calculator==1.0.0',
